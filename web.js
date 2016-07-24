692d2cb849501e15ef7a10b5d6581cb44ebf1e04
Mohamed Ezzaouia <mohamed@speakplus.fr>
2016-07-24 21:06:25 +0200

    adding web server

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
+app.use(express.static(__dirname));
+
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/index.html"));
 app.listen(process.env.PORT || 5000);
